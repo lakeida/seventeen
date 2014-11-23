0c5eb6adde5919cb3e1bc5cf1bc6f15880b9d871
Cherie Tabb <Cherietabb@gmail.com>
2014-11-22 18:00:11 -0600

    created subjects

--- studyapp/config/routes.rb
+++ studyapp/config/routes.rb
@@ -1,4 +1,6 @@
 Studyapp::Application.routes.draw do
+  resources :subjects
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
